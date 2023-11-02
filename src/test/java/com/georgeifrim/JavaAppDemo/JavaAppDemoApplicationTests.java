fdf0b449c8323ff92f1b1b3f560f71f2001b4edc
George Ifrim <ifrim.george.alexandru@gmail.com>
2023-11-02 14:28:34 +0200

    Initial commit

---
+++ src/test/java/com/georgeifrim/JavaAppDemo/JavaAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeifrim.JavaAppDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
